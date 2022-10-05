e93233b2f71047fbf9b839d838c06252a388359a
Julia Iskandarova <juliaiskandarova2004@gmail.com>
2022-10-05 13:08:27 +0300

    Add new file

---
+++ 03_cpp_intro/CMakeLists.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int a;
+int main()
+{
+	string a;
+	cout << "Write your name, please \n";
+	cin >> a;
+	cout << "Hello, " << a << "!";
+	system("pause");
+	return 0;
+}
