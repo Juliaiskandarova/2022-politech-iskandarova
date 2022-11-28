912f591f9ff32012960cc9a4ad8c00ff79fda7d3
Julia Iskandarova <juliaiskandarova2004@gmail.com>
2022-11-28 15:59:25 +0300

    Add text

---
+++ 05_computer_architecture/CMakeLists.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	char parameter[20];   
+	char unit[3];  
+	double memory[2];
+
+	ifstream in("/proc/meminfo", std::ios_base::in);   
+	for (int i = 0; i < 2; i++)
+	{
+		in >> parameter >> value >> unit;
+		memory[i] = value;
+		cout << memory[i] << endl;
+	}
+	double p = memory[1] / memory[0];
+	int percentage = p * 100;
+	for (int i = 0; i < percentage; i++)
+	{
+		cout << (char)64;
+	}
+	for (int i = 0; i < (100 - percentage); i++)
+	{
+		cout << (char)46;
+	}
+	cout << " " << percentage << "%" << endl;
+	return 0;
+
+}
